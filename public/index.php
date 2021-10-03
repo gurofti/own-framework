1e453b3f85b775b662203b7c0cae7c50dab00a90
Gürkan Güney <65256779+gurofti@users.noreply.github.com>
2021-10-03 22:21:40 +0300

    Migration Created
    Migration Created

--- public/index.php
+++ public/index.php
@@ -3,18 +3,34 @@
 use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\core\Application;
+use Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+$config = [
+    //'userClass' => \app\models\User::class,
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
 
-$app = new Application(dirname(__DIR__));
+$app = new Application(dirname(__DIR__), $config);
 
-$app->router->get('/', [new SiteController, 'home']);
-$app->router->get('/contact', [new SiteController, 'contact']);
-$app->router->post('/contact', [new SiteController, 'handleContact']);
+// $app->on(Application::EVENT_BEFORE_REQUEST, function(){
+//     echo "Before request from second installation";
+// });
 
-$app->router->get('/login', [new AuthController(), 'login']);
-$app->router->post('/login', [new AuthController(), 'login']);
-$app->router->get('/register', [new AuthController(), 'register']);
-$app->router->post('/register', [new AuthController(), 'register']);
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/register', [SiteController::class, 'register']);
+$app->router->post('/register', [SiteController::class, 'register']);
+$app->router->get('/login', [SiteController::class, 'login']);
+$app->router->post('/login', [SiteController::class, 'login']);
+$app->router->get('/logout', [SiteController::class, 'logout']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+//$app->router->get('/about', [AboutController::class, 'index']);
+$app->router->get('/profile', [SiteController::class, 'profile']);
 
 $app->run();

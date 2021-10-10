c1d086d77c2618378f7f77273c0066db5a0032b6
Gürkan Güney <65256779+gurofti@users.noreply.github.com>
2021-10-10 14:48:44 +0300

    Implement ActiveRecord & Finish Registration
    Implement ActiveRecord & Finish Registration

--- public/index.php
+++ public/index.php
@@ -12,9 +12,21 @@ $app->router->get('/', [new SiteController, 'home']);
 $app->router->get('/contact', [new SiteController, 'contact']);
 $app->router->post('/contact', [new SiteController, 'handleContact']);
 
+<<<<<<< Updated upstream
 $app->router->get('/login', [new AuthController(), 'login']);
 $app->router->post('/login', [new AuthController(), 'login']);
 $app->router->get('/register', [new AuthController(), 'register']);
 $app->router->post('/register', [new AuthController(), 'register']);
+=======
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/logout', [AuthController::class, 'logout']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+//$app->router->get('/about', [AboutController::class, 'index']);
+$app->router->get('/profile', [SiteController::class, 'profile']);
+>>>>>>> Stashed changes
 
 $app->run();
